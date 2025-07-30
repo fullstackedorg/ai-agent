d0e815f7542d008e13c1b4efc226b4e2f9781baa
cplepage <lepage.charlesphilippe@gmail.com>
2025-07-30 11:41:18 -0400

    better export

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { createConversation } from "./conversation";
+import { createOllama } from "./providers/ollama";
+import { createOpenAI } from "./providers/openai";
+
+export { createConversation, createOllama, createOpenAI };
